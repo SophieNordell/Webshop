a6c0a317b3e7ffc745c19fc507e7f911607ddae9
maricolous <malin@malinnordlund.se>
2025-02-24 15:36:05 +0100

    cartPage design

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,36 @@
+import React from "react";
+import "../Cart.css";
+
 const Cart = () => {
-  <>
-    <div></div>
-  </>;
+  return (
+    <main>
+      <section className="cartWrap">
+        <h1>Kundvagn</h1>
+        <article className="cartProduct">
+          <div className="productImage">
+            <img src=""></img>
+          </div>
+          <div className="productInfo">
+            <h3>Product Title</h3>
+            <p className="productDescription">Description</p>
+            <p className="price">1000 kr</p>
+          </div>
+          <div className="productCounter">
+            <button className="counterButton">-</button>
+            <span className="productCount">1</span>
+            <button className="counterButton">+</button>
+          </div>
+        </article>
+        <section className="cartFooter">
+          <div className="totalAmount">Totalt: 1000 kr</div>
+          <div className="cartNav">
+            <button className="greyButton">FORTSÄTT HANDLA</button>
+            <button className="redButton">GÅ VIDARE</button>
+          </div>
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default Cart;
