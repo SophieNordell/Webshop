68e9b549fdfe1e91e57dcb396bfb9ad2d8e32884
dkmathisen <dennis.mathisen@chasacademy.se>
2025-02-26 13:28:28 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import Products from "./pages/products";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/confirmation" element={<Confirmation />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
+      <Footer />
     </>
   );
 };
