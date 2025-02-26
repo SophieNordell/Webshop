cce80519ed66e775249736ed763fbaef41ab48ad
dkmathisen <dennis.mathisen@chasacademy.se>
2025-02-26 11:28:05 +0100

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,8 @@ import ProductCard from "./components/productCard";
 import Home from "./pages/Home";
 import Products from "./pages/products";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
-/* import { Route, Routes } from "react-router-dom";
- */
-=======
+import { Route, Routes } from "react-router-dom";
 
-import { Routes, Route } from "react-router-dom";
-
-
-
-
->>>>>>> 90a1dc53060f75c2b68258b45ca51a942b56f648
 const App = () => {
   return (
     <>
