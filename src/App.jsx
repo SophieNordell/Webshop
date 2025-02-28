c0ffc437e1a161706f7755b2a40d0adea4b896a4
dkmathisen <dennis.mathisen@chasacademy.se>
2025-02-28 13:39:57 +0100

    confirmation update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Cart from "./pages/Cart";
 import Confirmation from "./pages/Confirmation";
 import ProductCard from "./components/ProductCard";
 import Home from "./pages/Home";
-import Products from "./pages/products";
+import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
