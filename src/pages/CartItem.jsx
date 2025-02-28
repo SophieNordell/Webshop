a8241ee204e9b993195b0bf457cd77107690bda8
maricolous <malin@malinnordlund.se>
2025-02-28 14:21:48 +0100

    Divided cart into components

---
+++ src/pages/CartItem.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CartItem = ({ item, increaseQuantity, decreaseQuantity, removeItem }) => {
+  return (
+    <article className="cartProduct">
+      <div className="productImage">
+        <img src={item.image} alt={item.title} />
+      </div>
+      <div className="productInfo">
+        <Link to={`/product/${item.id}`}>
+          <h3>{item.title}</h3>
+        </Link>
+        <span className="price">{item.price} Kr</span>
+      </div>
+      <div className="productCounter">
+        <button
+          className="counterButton"
+          onClick={() => decreaseQuantity(item.id)}
+          disabled={item.quantity === 1}
+        >
+          -
+        </button>
+        <span className="productCount">{item.quantity} st</span>
+        <button
+          className="counterButton"
+          onClick={() => increaseQuantity(item.id)}
+        >
+          +
+        </button>
+        <button className="removeButton" onClick={() => removeItem(item.id)}>
+          X
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
---
+++ src/pages/CartSummary.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CartSummary = ({ totalSum, handleProceed }) => {
+  return (
+    <div className="cartFooter">
+      <div className="totalAmount">Totalt: {totalSum} SEK</div>
+      <div className="cartNav">
+        <Link className="greyButton" to="/">
+          Fortsätt handla
+        </Link>
+        <Link className="redButton" to="/Userinputs" onClick={handleProceed}>
+          Gå vidare
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CartSummary;
