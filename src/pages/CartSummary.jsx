de1b2c609bc8f17a675485f6c8d293bbc431d236
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2025-03-01 17:08:10 +0100

    fixade cart och cartsummery + warningsMeddelande

--- src/pages/CartSummary.jsx
+++ src/pages/CartSummary.jsx
@@ -1,34 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "../components/Button";
+const CartSummary = ({ cart, totalSum, handleProceed }) => {
+  const [warning, setWarning] = useState("");
+  const isCartEmpty = cart.length === 0;
 
-const CartSummary = ({ totalSum, handleProceed, isCartEmpty }) => {
+  const handleProceedClick = (e) => {
+    if (isCartEmpty) {
+      e.preventDefault();
+      setWarning("Varukorgen är tom! Fortsätt handla för att gå vidare.");
+
+      setTimeout(() => {
+        setWarning("");
+      }, 2000);
+
+      return;
+    }
+    handleProceed();
+  };
+
   return (
     <div className="cartFooter">
       <div className="totalAmount">Totalt: {totalSum} SEK</div>
       <div className="cartNav">
-        {/* Link för "Fortsätt handla" knappen */}
         <Link className="greyButton" to="/products">
           <Button className="greyButton">Fortsätt handla</Button>
         </Link>
 
-        {/* Button för "Gå vidare" - inaktiv om varukorgen är tom */}
         <Button
-          to="/Userinputs"
           className="redButton"
-          onClick={handleProceed}
-          disabled={isCartEmpty} // Om varukorgen är tom, gör knappen inaktiv
+          onClick={handleProceedClick}
+          disabled={isCartEmpty}
+          to={!isCartEmpty ? "/Userinputs" : undefined}
         >
           Gå vidare
         </Button>
       </div>
-
-      {/* Om varukorgen är tom, visa ett meddelande */}
-      {isCartEmpty && (
-        <div className="empty-cart-message">Varukorgen är tom!</div>
-      )}
+      {warning && <div className="warningMessage">{warning}</div>}
     </div>
   );
 };
-
-export default CartSummary;
