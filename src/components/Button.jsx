51e11ca51c997fc1c342767798cc4a794bee0d1c
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2025-03-05 11:00:07 +0100

    fixade imports

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../button.css";
+import "/src/button.css";
 
 const Button = ({ children, onClick, className = "", to }) => {
   const buttonClass = `${className}`;
